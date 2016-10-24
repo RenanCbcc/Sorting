3afa153efb09667a8beda53111b6404900ff4f5b
Renan <Renan@192.168.0.9>
2016-10-24 16:27:03 -0300

    Bubble_sort()

---
+++ src/principal/Main.java
@@ -0,0 +1,12 @@
+package principal;
+import simple_sorting.Bubble_sort;
+
+public class Main {
+	private int MAX = 100;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/simple_sorting/Insertion_sort.java
+++ src/simple_sorting/Insertion_sort.java
@@ -1,5 +1,8 @@
 package simple_sorting;
-
+/* Insertion sort é prefirível ao Quick sort para pastas pequenas
+ * e para pastas pequenas parcialmente ordenadas. De fato, Insertion
+ * sort é comumente usado como parte do Quick sort. 
+ */
 public class Insertion_sort {
 
 }
