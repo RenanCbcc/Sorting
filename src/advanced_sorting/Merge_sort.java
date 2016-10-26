1404db01107f0f5cfd344787c678a1bd69a727ef
Renan <Renan@192.168.0.7>
2016-10-26 16:04:42 -0300

    Merge_sort()

--- src/advanced_sorting/Merge_sort.java
+++ src/advanced_sorting/Merge_sort.java
@@ -1,5 +1,71 @@
 package advanced_sorting;
 
 public class Merge_sort {
-
+	
+		private int[] array;
+		private int elements;
+		
+		//----------------------------------------------------------
+		
+		public Merge_sort(int max)
+		{
+			array = new int[max];
+			elements = 0;
+		}
+		
+		//----------------------------------------------------------
+		
+		public void inserte(int value)
+		{
+			array[elements] = value;
+			elements ++;
+		}
+		
+		//----------------------------------------------------------
+		
+		public void display()
+		{
+			for (int i = 0; i < elements; i++)
+			{
+				System.out.println(array[i]);
+			}
+		}
+		
+		//----------------------------------------------------------
+		
+		public void MergeSort()
+		{
+			int[] workspace = new int[elements];
+			MergeSort(workspace,0,elements-1);
+		}
+		
+		public void MergeSort(int[] array,int lowerBound, int upperBound)
+		{
+			if (lowerBound == upperBound )
+			{
+				return; //caso base,erray com único elemento já está ordenado.
+			}
+			
+			else
+			{
+				int mid = (lowerBound + upperBound)/2; //encontra um ponto intermediário.
+				MergeSort(array,lowerBound,mid);//metade inferior
+				MergeSort(array,mid +1,upperBound);//metade superior
+				
+			}
+		}
+		
+		public void merge(int array, int lowPointer,int highPointer,int upperBound)
+		{
+			int j = 0;
+			int lowerBound = lowPointer;
+			int mid = highPointer -1;
+			int n = upperBound - lowerBound + 1; // # of itens
+			
+			while(lowPointer <= mid && highPointer <= upperBound)
+			{
+				if ( a)
+				array[j++]
+			}
+		}
 }
